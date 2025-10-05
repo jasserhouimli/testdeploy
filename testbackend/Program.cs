c3da2ca7eabc3250820163bb1df04a534cac2652
Jasser Houimli <108799236+jasserhouimli@users.noreply.github.com>
2025-10-05 15:24:23 +0100

    sqgqsfg

--- testbackend/Program.cs
+++ testbackend/Program.cs
@@ -8,14 +8,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
 builder.Services.AddControllers();
-//builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddEndpointsApiExplorer();
 
 
 // Add SignalR
-
+builder.Services.AddSignalR();
 
 // Add services
-
+builder.Services.AddSingleton<RoomService>();
 
 var app = builder.Build();
 
@@ -26,14 +26,14 @@ if (app.Environment.IsDevelopment())
 }
 
 //app.UseHttpsRedirection();
-//app.UseStaticFiles();
-//app.UseRouting();
-
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
 
 app.MapControllers();
-
+app.MapHub<TypeRaceHub>("/typeracehub");
 
 // Serve index.html as default page
-//app.MapFallbackToFile("index.html");
+app.MapFallbackToFile("index.html");
 
 app.Run();
